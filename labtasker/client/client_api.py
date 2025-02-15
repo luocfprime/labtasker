c92af9aef02a7df8242be895440433f8ef944fb2
lcf <kevinfinley@163.com>
2025-02-16 00:40:20 +0800

    (update) client api

--- labtasker/client/client_api.py
+++ labtasker/client/client_api.py
@@ -1,10 +1,15 @@
 from labtasker.client.core.api import *
+from labtasker.client.core.context import current_task_id, current_worker_id, task_info
 from labtasker.client.core.job_runner import finish, loop
 
 __all__ = [
     # job runner api
     "loop",
     "finish",
+    # context api
+    "task_info",
+    "current_task_id",
+    "current_worker_id",
     # http api (you should be careful with these unless you know what you are doing)
     "close_httpx_client",
     "health_check",
