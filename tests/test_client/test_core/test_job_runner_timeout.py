d3769d5f43bb2754ac8f68f6e4af52c1e0703255
lcf <kevinfinley@163.com>
2025-02-18 15:04:24 +0800

    (test) job runner test update

--- tests/test_client/test_core/test_job_runner_timeout.py
+++ tests/test_client/test_core/test_job_runner_timeout.py
@@ -1,20 +1,24 @@
+"""
+These tests test timeout behavior in end-to-end tests.
+Note: they are quite time-consuming.
+"""
+
+import os
 import time
 
 import pytest
 
-from labtasker import create_queue, finish, loop, ls_tasks, submit_task, task_info
+from labtasker import create_queue, loop, ls_tasks, submit_task
 from labtasker.client.core.context import set_current_worker_id
+from labtasker.client.core.paths import get_labtasker_log_dir
 from tests.fixtures.logging import silence_logger
 
 pytestmark = [
-    pytest.mark.unit,
-    pytest.mark.integration,
     pytest.mark.e2e,
     pytest.mark.usefixtures(
         "silence_logger"
     ),  # silence logger in testcases of this module
 ]
-
 TOTAL_TASKS = 5
 
 
@@ -25,6 +29,11 @@ def setup_queue(client_config):
         password=client_config.password.get_secret_value(),
         metadata={"tag": "test"},
     )
+
+
+@pytest.fixture(autouse=True)
+def reset_worker_id():
+    set_current_worker_id(None)
 
 
 @pytest.fixture
@@ -40,96 +49,42 @@ def setup_tasks(db_fixture):
         )
 
 
-@pytest.fixture(autouse=True)
-def reset_worker_id():
-    set_current_worker_id(None)
+def delay(interval: float):
+    cycles = int(interval // 0.01)
+    for _ in range(cycles):
+        time.sleep(0.01)
 
 
-def test_job_success(setup_tasks):
-    tasks = ls_tasks()
-    assert tasks.found
-    assert len(tasks.content) == TOTAL_TASKS
-
-    idx = -1
-
-    @loop(required_fields=["arg1", "arg2"], eta_max="1h", pass_args_dict=True)
-    def job(args):
-        nonlocal idx
-        idx += 1
-        task_name = task_info().task_name
-        assert task_name == f"test_task_{idx}"
-        assert args["arg1"] == idx
-        assert args["arg2"]["arg3"] == idx
-
-        time.sleep(0.1)  # a tiny delay to ensure the tasks api request are processed
-
-        finish("success")
-
-    job()
-
-    assert idx + 1 == TOTAL_TASKS, idx
-
-    tasks = ls_tasks()
-    assert tasks.found
-    for task in tasks.content:
-        assert task.status == "success"
+def trigger_heartbeat_thread_termination():
+    # This is a bit of a hack.
+    # The heartbeat thread will stop once the lock file is deleted.
+    # Only use this for testing. Do not use this practice.
+    heartbeat_lock = get_labtasker_log_dir() / f"heartbeat.lock"
+    if heartbeat_lock.exists():
+        os.unlink(heartbeat_lock)
 
 
-def test_job_manual_failure(setup_tasks):
+def test_job_task_timeout(setup_tasks, server_config):
     cnt = 0
 
-    max_retries = 3
+    max_retries = 1  # only 1 attempt, since it is quite time-consuming
+
+    timeout = server_config.periodic_task_interval / 2
 
     @loop(
         required_fields=["arg1", "arg2"],
-        eta_max="1h",
+        eta_max=f"{timeout}sec",  # set task timeout
         create_worker_kwargs={"max_retries": max_retries},
         pass_args_dict=True,
     )
     def job(args):
         nonlocal cnt
         cnt += 1
-        time.sleep(0.1)  # a tiny delay to ensure the tasks api request are processed
-        finish("failed")
+        delay(timeout * 3)
 
     job()
 
-    assert cnt == max_retries, cnt
-
-    tasks = ls_tasks()
-    assert tasks.found
-
-    total_retries = 0
-    for task in tasks.content:
-        # all failed tasks should be rejoined into the queue
-        # since the most recently failed task will join at the end
-        assert task.status == "pending"
-        total_retries += task.retries
-
-    assert total_retries == max_retries, total_retries
-
-
-def test_job_auto_failure(setup_tasks):
-    cnt = 0
-
-    max_retries = 3
-
-    @loop(
-        required_fields=["arg1", "arg2"],
-        eta_max="1h",
-        create_worker_kwargs={"max_retries": max_retries},
-        pass_args_dict=True,
-    )
-    def job(args):
-        nonlocal cnt
-        cnt += 1
-        time.sleep(0.1)  # a tiny delay to ensure the tasks api request are processed
-
-        assert False  # the exception should be caught in loop and auto reported
-
-    job()
-
-    assert cnt == max_retries, cnt
+    assert cnt == max_retries, str(cnt)
 
     tasks = ls_tasks()
     assert tasks.found
@@ -137,3 +92,39 @@ def test_job_auto_failure(setup_tasks):
         # all failed tasks should be rejoined into the queue
         # since the most recently failed task will join at the end
         assert task.status == "pending"
+
+    # at least one of the tasks should have retries > 0
+    assert not all(task.retries == 0 for task in tasks.content), str(tasks.content)
+
+
+def test_job_heartbeat_timeout(setup_tasks, server_config):
+    cnt = 0
+
+    max_retries = 1  # only 1 attempt, since it is quite time-consuming
+
+    timeout = server_config.periodic_task_interval / 2
+
+    @loop(
+        required_fields=["arg1", "arg2"],
+        heartbeat_timeout=timeout,
+        create_worker_kwargs={"max_retries": max_retries},
+        pass_args_dict=True,
+    )
+    def job(args):
+        nonlocal cnt
+        cnt += 1
+        trigger_heartbeat_thread_termination()  # stop heartbeat (hack)
+        delay(timeout * 3)
+
+    job()
+
+    assert cnt == max_retries, str(cnt)
+
+    tasks = ls_tasks()
+    assert tasks.found
+    for task in tasks.content:
+        # all failed tasks should be rejoined into the queue
+        # since the most recently failed task will join at the end
+        assert task.status == "pending"
+
+    assert not all(task.retries == 0 for task in tasks.content), str(tasks.content)
