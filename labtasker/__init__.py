8eb43618f89fed4163b07aba8d437c196e013ee3
lcf <kevinfinley@163.com>
2025-03-06 16:49:15 +0800

    (chore) Version 0.1.1 -> 0.1.2

--- labtasker/__init__.py
+++ labtasker/__init__.py
@@ -4,7 +4,7 @@ from labtasker.client.core.exceptions import *
 from labtasker.client.core.paths import get_labtasker_client_config_path
 from labtasker.filtering import install_traceback_filter, set_traceback_filter_hook
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 install_traceback_filter()
 
