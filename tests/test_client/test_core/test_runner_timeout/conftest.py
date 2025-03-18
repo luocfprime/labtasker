ba8032debabc65fa84e17d68d1770023465bd9a4
lcf <kevinfinley@163.com>
2025-03-18 19:45:35 +0800

    (fix) Fixed the timeout test setup_loop_internal_error_handler to expect LabtaskerRuntimeError("Current task is assigned to a different worker")

--- tests/test_client/test_core/test_runner_timeout/conftest.py
+++ tests/test_client/test_core/test_runner_timeout/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+from labtasker.client.core.exceptions import LabtaskerRuntimeError
 from labtasker.client.core.job_runner import (
     _loop_internal_error_handler,
     set_loop_internal_error_handler,
@@ -15,6 +16,16 @@ def setup_loop_internal_error_handler():
         # give you a "500: InvalidStateTransition: Cannot transition from pending to success."
         if "InvalidStateTransition" in str(e) and "from pending to" in str(e):
             return
+
+        # When timeout job loop eventually reports task status
+        # after exceeding a timeout period, the job could be assigned to a different worker (or the worker_id set to None)
+        # Either way, it would trigger a LabtaskerRuntimeError, which is the expected behaviour.
+        # We need to ignore this error.
+        if isinstance(
+            e, LabtaskerRuntimeError
+        ) and "Current task is assigned to a different worker" in str(e):
+            return
+
         raise e
 
     original_handler = _loop_internal_error_handler
