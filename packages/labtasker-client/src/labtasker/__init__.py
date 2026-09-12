ba0a930e5260ceef4c1c3e653de88ef90a4ee15b
lcf <luocfprime@gmail.com>
2026-09-13 00:06:25 +0800

    release: prepare v2.3.0

--- packages/labtasker-client/src/labtasker/__init__.py
+++ packages/labtasker-client/src/labtasker/__init__.py
@@ -48,7 +48,7 @@ from labtasker.models import (
 from labtasker.types import JSONValue, TaskOrderField, TaskStatus, TaskUpdate
 from labtasker.worker import loop
 
-__version__ = "2.2.0"
+__version__ = "2.3.0"
 
 __all__ = [
     "APIError",
